ff7f0076916f24421fd2c51971b49bf0fc663ed9
dani limbeck <dani.limbeck@gmx.at>
2023-05-08 21:16:25 +0200

    writes x and o in loop while number correct, but replaces occupied places with new x or o;

--- tictactoe.py
+++ tictactoe.py
@@ -4,17 +4,17 @@ def player_move(board):
     # Returns the game board with the given mark in the given position.
     position = int(input("enter position for you mark: "))
     mark = input("enter mark: ")
-    board = board
     
-    while board[position] == "-":
+    if board[position] == "-":
         if 0 <= position <= 20:
-            board = board[0:(position - 1)] + mark + board[(position + 1):20]
+            board = board[0:(position - 1)] + mark + board[(position):20]
         else:
-            position = 0
             print("no valid value")
-    else:        
+    else:       
         print("sorry occupied")
     return(board)
+
+
  
 
 def pc_move(board):
@@ -23,7 +23,7 @@ def pc_move(board):
     mark = "o"
     if board[position] == "-":
         if 0 <= position <= 20:
-            board = board[0:(position - 1)] + mark + board[(position + 1):20]
+            board = board[0:(position - 1)] + mark + board[(position):20]
             return(board)
         else:
             print("no valid value")
@@ -31,10 +31,11 @@ def pc_move(board):
         print("sorry occupied")
 
 
-
 board = "-"*20
-
-board = player_move(board)
-print(board)
-board = pc_move(board)
-print(board)
+while board.count("-") > 0:
+    board = player_move(board)
+    print(board)
+    board = pc_move(board)
+    print(board)
+else:
+    print("finish")
