e03561cbf59209d71c04e9676b9a90c305bce824
dani limbeck <dani.limbeck@gmx.at>
2023-05-08 21:47:58 +0200

    still overwrites position...

--- tictactoe.py
+++ tictactoe.py
@@ -5,36 +5,33 @@ def player_move(board):
     position = int(input("enter position for you mark: "))
     mark = input("enter mark: ")
     
-    if board[position] == "-":
+    if (board[position]) != "-":
+        print("sorry occupied")
+    else:       
         if 0 <= position <= 20:
             board = board[0:(position - 1)] + mark + board[(position):20]
         else:
             print("no valid value")
-    else:       
-        print("sorry occupied")
     return(board)
 
-
- 
-
 def pc_move(board):
-    board = board
     position = randrange(0,19)
+    print(position)
     mark = "o"
-    if board[position] == "-":
+
+    if (board[position]) != "-":
+        print("sorry occupied")
+    else:       
         if 0 <= position <= 20:
             board = board[0:(position - 1)] + mark + board[(position):20]
-            return(board)
         else:
             print("no valid value")
-    else:
-        print("sorry occupied")
+    return(board)
 
 
 board = "-"*20
 while board.count("-") > 0:
     board = player_move(board)
-    print(board)
     board = pc_move(board)
     print(board)
 else:
